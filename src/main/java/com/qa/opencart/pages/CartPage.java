03a649285a83c8fc65378fa7f061b06f52387c34
aajaykumardinne <dinnekumar@outlook.com>
2022-06-15 12:17:13 +0530

    added cart and updated login page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,23 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+	
+	// 1. declare WebDriver as private
+	private WebDriver driver;
+	
+	
+	// 2. initialising the Webdriver 
+	public CartPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	// 3. by locators
+	
+	
+	
+	
+
+}
